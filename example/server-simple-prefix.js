15b0316fb993ebb2462668db0de9e6ffd4c70ac6
IndieForger <indieforger@gmail.com>
2016-02-28 08:56:56 +0000

    example/server-simple-prefix.js

---
+++ example/server-simple-prefix.js
@@ -0,0 +1,55 @@
+'use strict'
+/* --------------------------------
+Before your run this example
+1. Register twitter application
+2. Update config.json with authentication credentials
+3. Run `npm install`
+4. Run demo with: `node server-simple-prefix.js`
+---------------------------------- */
+const
+	Hapi = require('hapi'),
+	Boom = require('boom'),
+	Hoek = require('hoek'),
+	config = require('./config.json'),
+	server = new Hapi.Server()
+
+// plugins registration complete handler
+function onRegister(err) {
+  Hoek.assert(!err, err)
+
+  // auto redirect to twitter login route from main one
+	server.route({
+		method: 'GET',
+		path: '/',
+		config: {
+			handler: function (request, reply) {
+        reply.redirect('user/login/twitter')
+      }
+		}
+	})
+
+	// Start the server
+	server.start((err) => {
+		if (err) throw err
+		console.log('Server running at:', server.info.uri)
+	})
+}
+
+// setup connections
+server.connection({
+	port: config.port || process.env.PORT || 3000,
+	host: config.host || process.env.HOST || 'localhost'
+})
+
+// register required plugins
+server.register(require('bell'), function (err) {
+  Hoek.assert(!err, err)
+
+  // important!
+  // make configuration available to plugin
+  server.app.config = config
+
+  server.register(require('../lib/login-plugin'), {
+    routes: { prefix: '/user' }
+  }, onRegister)
+})
